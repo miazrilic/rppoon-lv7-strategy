e96bc90ef737ad8355003ea106754e53ce1cd602
miazrilic <mia.zrilic@gmail.com>
2024-05-28 00:02:16 +0200

    Add project files.

---
+++ BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class BubbleSort : SortStrategy
+    {
+        public override void Sort(double[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j+1]);
+                    }
+                }
+            }
+
+        }
+    }
+}
+        
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortStrategy bubble = new BubbleSort();
+            SortStrategy sequentialSort = new SequentialSort();
+
+            NumberSequence sequence = new NumberSequence(6, bubble);
+            sequence.InsertAt(0, 2);
+            sequence.InsertAt(1, 4);
+            sequence.InsertAt(2, 1);
+            sequence.InsertAt(3, 3);
+            sequence.InsertAt(4, 6);
+            sequence.InsertAt(5, 5);
+            sequence.Sort();
+            Console.WriteLine(sequence);
+
+            sequence.InsertAt(0, 0);
+
+
+
+            
+            sequence.SetSortStrategy(sequentialSort);
+            sequence.Sort();
+            
+            Console.WriteLine(sequence);
+
+            
+
+            
+
+            
+
+            
+        }
+    }
+    }
